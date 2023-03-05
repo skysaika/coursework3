0ecb44751f447d43f51e99424f5fd354271ff681
Saysuu <skysaika@yandex.ru>
2023-03-05 19:23:13 +0300

    added sender, recipient

--- main.py
+++ main.py
@@ -4,15 +4,19 @@ from utils.utils import get_data, get_filtered_data, get_last_data, get_formatte
 def main():
     DATA_JSON = "https://file.notion.so/f/s/d22c7143-d55e-4f1d-aa98-e9b15e5e5efc/operations.json?spaceId=0771f0bb-b4cb-4a14-bc05-94cbd33fc70d&table=block&id=f11058ed-10ad-42ea-a13d-aad1945e5421&expirationTimestamp=1678104559111&signature=zWzdiPwc4jR19dH5kXSEDL_-w6jYQ7XBtMj0TqkJUko&downloadName=operations.json"
     COUNT_LAST_VALUES = 5
+    FILTERED_EMPTY_FROM = True
 
     data, info = get_data(DATA_JSON)  # list of transactions
     if not data:
         exit(info)
-    print(info)
+    print(info, end="\n\n")
 
-    data = get_filtered_data(data)  # var of filtered transactions
+    data = get_filtered_data(data, FILTERED_EMPTY_FROM)  # var of filtered transactions
     data = get_last_data(data, COUNT_LAST_VALUES)  # address to get last 5 values
     data = get_formatted_data(data)  # formatted data
+    for row in data:
+        print(row)
+
 
 if __name__ == '__main__':
     main()
--- utils/utils.py
+++ utils/utils.py
@@ -1,4 +1,5 @@
 import requests
+from datetime import datetime
 
 
 def get_data(url):
@@ -12,9 +13,11 @@ def get_data(url):
         return None, "ERROR: Ошибка соединения с сервером"
 
 
-def get_filtered_data(data):
+def get_filtered_data(data, filtered_empty_from=False):
     """function to get filtered data"""
     data = [x for x in data if "state" in x and x["state"] == 'EXECUTED']
+    if filtered_empty_from:
+        data = [x for x in data if "from" in x]
     return data
 
 
@@ -28,5 +31,19 @@ def get_formatted_data(data):
     """function to get formatted data"""
     formatted_data = []
     for row in data:
-        print(row)
-        return
+        date = datetime.strptime(row["date"], "%Y-%m-%dT%H:%M:%S.%f").strftime("%d.%m.%Y")
+        description = row["description"]
+
+        sender = row["from"].split()
+        sender_bill = sender.pop(-1)  # separate bill from name card
+        sender_bill = f"{sender_bill[:4]} {sender_bill[4:6]} ** **** {sender_bill[-4:]}"
+        sender_info = " ".join(sender)  # join list to string
+
+        recipient = f"**{row['to'][-4:]}"
+        amount = f'{row["operationAmount"]["amount"]} {row["operationAmount"]["currency"]["name"]}'
+        formatted_data.append(f"""\
+{date} {description} 
+{sender_info} {sender_bill}  -> Счет {recipient} 
+{amount}
+""")
+    return formatted_data
